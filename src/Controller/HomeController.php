59d80925082063a44ca19267a478f79a9b69ce40
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-09-20 15:28:27 +0200

    search by name

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,10 +20,21 @@ class HomeController extends AbstractController
     }
 
     #[Route('/blog', name: 'app_blog')]
-    public function blog(): Response
+    public function blog(EntityManagerInterface $entityManager, Request $request): Response
     {
+
+        $postEntity = $entityManager->getRepository(Post::class);
+        if ($request->query->get('q')) {
+            $posts = $postEntity->createQueryBuilder('p')
+                ->where('p.title LIKE :title')
+                ->setParameter('title', '%' . $request->query->get('q') . '%')
+                ->getQuery()
+                ->getResult();
+        } else {
+            $posts = $postEntity->findAll();
+        }
         return $this->render('blog/blog.html.twig', [
-            'controller_name' => 'HomeController',
+            'posts' => $posts,
         ]);
     }
 }
