c434dad35ba9104d8f469d6c552979cddd8e7095
alex <alexandre.souvorov@edu.esiee-it.fr>
2023-09-21 11:22:15 +0200

    Comments done

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\Tag;
+use App\Entity\User;
+use App\Form\CommentType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
@@ -58,6 +65,47 @@ class HomeController extends AbstractController
         return $this->render('blog/blog.html.twig', [
             'posts' => $posts,
             'tags' => $allTags,
+        ]);
+    }
+
+    #[Route('/comment/{postId}/new', name: 'app_add_comment', methods: ['POST'])]
+    #[IsGranted('IS_AUTHENTICATED')]
+    public function commentNew(
+        #[CurrentUser] User $user,
+        Request $request,
+        #[MapEntity(mapping: ['postId' => 'id'])] Post $post,
+        EntityManagerInterface $entityManager,
+    ): Response {
+        $comment = new Comment();
+        $comment->setAuthor($user);
+        $post->addComment($comment);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPublicatedAt(new \DateTimeImmutable('now', new \DateTimeZone('Europe/Paris')));
+            $entityManager->persist($comment);
+            $entityManager->flush();
+            $this->addFlash('message', 'Commentaire ajouté avec succès');
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }else{
+            $this->addFlash('error', 'Une erreur est survenue lors de l\'ajout du commentaire');
+        }
+
+        return $this->render('post/_comment_form_error.html.twig', [
+            'post' => $post,
+            'form' => $form,
+        ]);
+    }
+
+    public function commentForm(Post $post): Response
+    {
+        $form = $this->createForm(CommentType::class);
+
+        return $this->render('post/_form_comment.html.twig', [
+            'post' => $post,
+            'form' => $form,
         ]);
     }
 }
