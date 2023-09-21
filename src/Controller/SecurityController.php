0b39b233eeb729107a2e682483ad5ea6422e0382
Lucastor05 <lucasdeniel05@gmail.com>
2023-09-21 21:45:18 +0200

    log login/register done ->next logout

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,13 +2,18 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Event\LogoutEvent;
 
 class SecurityController extends AbstractController
 {
+    public function __construct(private readonly LoggerInterface $logger)
+    {
+    }
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -24,8 +29,10 @@ class SecurityController extends AbstractController
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
+    #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
     {
+        $this->logger->info(" vient de se déconnécter !");
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
